496602ab1b8d1533185aeb6de81fd3d763d37755
djdembeck <dembeck.david@protonmail.com>
2026-02-09 17:11:56 -0600

    chore: initial project structure

---
+++ cmd/server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("audnexus-provider starting...")
+	log.Println("Server initialization complete")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/djdembeck/audnexus-provider
+
+go 1.22.2
